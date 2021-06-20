821f19d7648af1fdc48ae7db863dedbbc5b1cb24
FdI96 <federicolopezce@gmail.com>
2021-06-20 15:52:08 -0300

    Add menu.js file with the html block for menus and add event listeners to Home and Menu buttons in the navbar

---
+++ src/home.js
@@ -0,0 +1,22 @@
+const contentHomeComponent = () => {
+  const contentHome = document.createElement("div");
+  contentHome.setAttribute("class", "text-center bg-dark");
+
+  const title = document.createElement("h1");
+  title.setAttribute("class", "text-light");
+  title.innerHTML = "Brand New Cool Restaurant";
+
+  const imageRestaurant = document.createElement("img");
+  imageRestaurant.setAttribute("src", "../assets/images/restaurant.jpg");
+
+  const pRestaurant = document.createElement("p");
+  pRestaurant.innerHTML = "Welcome, this is my restaurant's interior ";
+
+  contentHome.appendChild(title);
+  contentHome.appendChild(imageRestaurant);
+  contentHome.appendChild(pRestaurant);
+
+  return contentHome;
+};
+
+export default contentHomeComponent;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import navBarComponent from "./navBarElement";
+import contentHomeComponent from "./home.js";
+import contentMenuComponent from "./menu.js";
 
+console.log();
 const content = document.getElementById("content");
 const nav = navBarComponent();
+const body = contentHomeComponent();
 
 content.appendChild(nav);
+content.appendChild(body);
+
+const bodyToMenu = document.getElementById("menuButton");
+bodyToMenu.addEventListener("click", () => {
+  const menu = contentMenuComponent();
+  content.removeChild(content.childNodes[1]);
+  content.appendChild(menu);
+});
+
+const bodyToHome = document.getElementById("menuHome");
+bodyToHome.addEventListener("click", () => {
+  const home = contentHomeComponent();
+  content.removeChild(content.childNodes[1]);
+  content.appendChild(home);
+});
